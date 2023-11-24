d8c08542b56d38b0fe39617499c2422601bd35e8
Azamat <easy28g>
2023-11-25 04:44:05 +0600

    assign grade

--- lib/pages/teacher_pages/AssignGradeToStudent.dart
+++ lib/pages/teacher_pages/AssignGradeToStudent.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'StudentsList.dart';
+
 class AssignGradeToStudent extends StatefulWidget {
   const AssignGradeToStudent({super.key});
 
@@ -8,6 +10,8 @@ class AssignGradeToStudent extends StatefulWidget {
 }
 
 class _AssignGradeToStudentState extends State<AssignGradeToStudent> {
+  final TextEditingController _textFieldController = TextEditingController();
+  final TextEditingController _textFieldControllerMark = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -16,8 +20,48 @@ class _AssignGradeToStudentState extends State<AssignGradeToStudent> {
         title: Text('VibeTime Күндөлүк'),
         centerTitle: true,
       ),
-      body: Text('AssignGradeToStudent'),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.fromLTRB(18.0, 35.0, 18.0, 0.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _textFieldController,
+              decoration: InputDecoration(
+                labelText: 'Описание',
+                border: OutlineInputBorder(),
+              ),
+              maxLines: null,
+            ),
+            SizedBox(height: 16.0),
+            // Просто добавьте немного отступа между полями
+            TextField(
+              controller: _textFieldControllerMark,
+              decoration: InputDecoration(
+                labelText: 'Оценка',
+                border: OutlineInputBorder(),
+              ),
+              maxLines: null,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          String enteredText = _textFieldController.text;
+          String markText = _textFieldControllerMark.text;
+          print('Entered Text1: $enteredText'); // терминал
+          print('Entered Text2: $markText'); // терминал
+          _textFieldController.clear();
+          _textFieldControllerMark.clear();
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => StudentsList(),
+            ),
+          );
+        },
+        child: Icon(Icons.check),
+      ),
     );
   }
-
 }
